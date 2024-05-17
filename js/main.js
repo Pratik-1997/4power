193fc73996fe64952a01ed59c65881e32b8f29d7
PratikInteractive <pratik.keluskar@interactiveavenues.com>
2024-05-17 17:14:44 +0530

    ixes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+// Header
+let prevScrollPos = $(window).scrollTop();
+const header = $("#sticky-header");
+let timer;
+function hideHeader() {
+  header.addClass("hidden");
+}
+function showHeader() {
+  clearTimeout(timer);
+  const currentScrollPos = $(window).scrollTop();
+  if (currentScrollPos < prevScrollPos || currentScrollPos === 0) {
+    header.removeClass("hidden");
+  } else {
+    header.addClass("hidden");
+    timer = setTimeout(hideHeader, 4000); // Adjust the timeout duration as needed (4000ms = 4 seconds)
+  }
+  prevScrollPos = currentScrollPos;
+}
+header.removeClass("hidden");
+$(window).scroll(showHeader);
+
 // Spotlight
 var swiper = new Swiper(".mySwiper", {
   pagination: {
@@ -13,7 +34,7 @@ var swiper = new Swiper(".mySwiper", {
 // Marquee Block
 const swiper1 = new Swiper(".companies.swiper", {
   loop: true,
-  slidesPerView: 5,
+  slidesPerView: 3,
   speed: 1000,
   spaceBetween: 10,
   autoplay: {
@@ -23,6 +44,12 @@ const swiper1 = new Swiper(".companies.swiper", {
     disableOnInteraction: true,
     waitForTransition: false,
   },
+  breakpoints: {
+    // when window width is >= 1024px (desktop)
+    1024: {
+      slidesPerView: 4,
+    },
+  },
 });
 
 // Additional styles to ensure smooth transitions
@@ -31,15 +58,38 @@ document.querySelector(".swiper-wrapper").style.transitionTimingFunction =
 
 // Hamburger
 $(document).ready(function () {
+  // Hamburger
   $(".ham").on("click", function () {
     $(this).toggleClass("active");
     $("nav").toggleClass("active");
+    $("body").toggleClass("overlayHidden");
   });
+
+  // Toggle Navigation
+
+  if ($(window).width() < 480) {
+    $(".has-child > a").click(function (event) {
+      event.preventDefault();
+
+      var $parentLi = $(this).parent(".has-child");
+      var $megaMenu = $parentLi.find(".mega-menu");
+
+      if ($parentLi.hasClass("active")) {
+        $megaMenu.slideUp(300);
+        $parentLi.removeClass("active");
+      } else {
+        $(".has-child .mega-menu").slideUp(300);
+        $(".has-child").removeClass("active");
+        $megaMenu.slideDown(300);
+        $parentLi.addClass("active");
+      }
+    });
+  }
 });
 
 // What We Do
 var swiper = new Swiper(".swiper.myWeDo", {
-  slidesPerView: '1.4',
+  slidesPerView: "1.4",
   spaceBetween: 30,
   loop: true,
   pagination: {
@@ -58,16 +108,15 @@ var swiper = new Swiper(".swiper.myWeDo", {
       slidesPerColumnFill: "row",
       loop: false,
       grid: {
-        rows: 3
+        rows: 3,
       },
     },
   },
 });
 
-
 // We Serve
 var swiper = new Swiper(".swiper.weServeSwiper", {
-  slidesPerView: '1.4',
+  slidesPerView: "1.4",
   spaceBetween: 30,
   loop: true,
   pagination: {
@@ -86,7 +135,7 @@ var swiper = new Swiper(".swiper.weServeSwiper", {
       slidesPerColumnFill: "row",
       loop: false,
       grid: {
-        rows: 2
+        rows: 2,
       },
     },
   },
